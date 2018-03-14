bb56ecdfa89404987df45fc347963cff13c47743
jezieljp <cccccc157@hotmail.com>
2018-03-14 09:44:13 -0300

    ERRO NO UGLIFY

---
+++ assets/src/js/text.js
@@ -0,0 +1,7 @@
+//PEGANDO ELEMENTO DO HTML
+var app = (function(document){
+    document.getElementById("title").innerHTML = "Curso de Gulp Dinamico";
+    document.getElementById("logo").innerHTML = "Gulp";
+    document.getElementById("logoDesc").innerHTML = "Autmatize o seu projeto Dinamico Com JavaScript";
+    document.getElementById("video").innerHTML = "Gulp-Uglify";    
+})(document);//Executando o script 
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function(){
+ return gulp.src('assets/src/sass/**/*.scss')//pega arquivo na pasta
+ .pipe(concat('style.min.css'))//renomeia o arquivo
+ .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+ .pipe(gulp.dest('assets/css'));//destino do sass para css 
+})
+
+//javascript
+gulp.task('js', function(){
+    return gulp.src('assets/src/js/**/*.js')
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+})
+
+//Obsevando as task e executando
+gulp.task('default', ['sass', 'js']);
+
