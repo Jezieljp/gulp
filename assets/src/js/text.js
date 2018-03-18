719555009897371a64ade0f05c2aa47b0a0065ba
jezieljp <cccccc157@hotmail.com>
2018-03-18 20:42:13 -0300

    FIM DO CURSO

--- assets/src/js/text.js
+++ assets/src/js/text.js
@@ -3,7 +3,6 @@ var app = (function(document){
     document.getElementById("title").innerHTML = "Curso de Gulp Dinamico";
     document.getElementById("logo").innerHTML = "Gulp";
     document.getElementById("logoDesc").innerHTML = "Autmatize o seu projeto Dinamico Com JavaScript";
-    document.getElementById("video").innerHTML = "Gulp-Uglify";    
+    document.getElementById("video").innerHTML = "Gulp-Watch";  
 })(document);//Executando o script 
 
-alert('holla mundo');
--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var browserSync = require("browser-sync").create();
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
 
 gulp.task('sass', function(){
  return gulp.src('assets/src/sass/**/*.scss')//pega arquivo na pasta
  .pipe(concat('style.min.css'))//renomeia o arquivo
  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
- .pipe(gulp.dest('assets/css'));//destino do sass para css 
-})
+ .pipe(gulp.dest('assets/css')) 
+ .pipe(browserSync.stream());
+});
 
 //javascript
 gulp.task('js', function(){
@@ -20,6 +26,37 @@ gulp.task('js', function(){
     .pipe(gulp.dest('assets/js'))
 })
 
+//Minificando o html
+gulp.task('htmlmin', function() {
+    return gulp.src('html/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('.'))
+
+});
+//liveReload
+gulp.task('server', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+  
+});
+
+//WATCH SERVE PARA OBSERVA MINHA TAREFAS
+gulp.task('watch', function(){
+    gulp.watch('assets/src/sass/**/*.scss', ['sass']);
+    gulp.watch('assets/src/js/**/*.js', ['js']);
+    gulp.watch('html/**/*.html', ['htmlmin']);
+});
+
+//MOSTRA ERROS NO JS
+gulp.task('lint', function(){
+    return gulp.src('assets/src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
 //Obsevando as task e executando
-gulp.task('default', ['sass', 'js']);
+gulp.task('default', ['sass', 'js', 'htmlmin', 'watch', 'server']);
 
